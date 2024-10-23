8b718863dcb5c6921007bde038aa4c5dc55cbe26
강동형 <buslaut@naver.com>
2024-10-23 16:44:00 +0900

    :tada: 프로젝트 시작

---
+++ frontend/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, theme } from './assets/styles/globalStyles';
+
+const queryClient = new QueryClient();
+
+const App: React.FC = () => {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        App
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+export default App;
