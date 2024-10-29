b7355c16425d9b5f9accc281dcc3c8494e365046
강동형 <buslaut@naver.com>
2024-10-29 15:02:11 +0900

    :sparkles: 프로젝트 선택 드롭다운 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
+import Select,{SingleValue} from "react-select"
 import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProvider } from "styled-components";
+import { OptionType } from "./Types/SelectType";
 
 //글로벌 css
 import { GlobalStyle, theme } from "./styles/GlobalStyled";
 
 //Styled 컴포넌트
-import { DisplayLayout, SidebarLayout, HeaderLayout, NavLayout } from "./App.Styled";
+import { DisplayLayout, SidebarLayout, HeaderLayout, NavLayout, ChoiceStyles } from "./App.Styled";
 
 import Sidebar from "./components/Sidebar";
 import IssuePage from "./pages/IssuePage";
@@ -23,8 +25,20 @@ const ParseHeader = (str:string)=>{
   return str.charAt(1).toUpperCase()+str.slice(2);
 }
 
+const tempOption = [
+  { value: 'Projec01',label:'Project01'},
+  { value: 'Projec02',label:'Project02'},
+  { value: 'Projec03',label:'Project03'}
+]
+
 const AppComponent: React.FC = () =>{
   const location = useLocation()
+
+  const [selectedOp,setSelectedOp] = useState<SingleValue<OptionType>>(null);
+
+  const handleChange = (option: SingleValue<OptionType>)=>{
+    setSelectedOp(option)
+  }
   return(
     <>
       <SidebarLayout>
@@ -33,6 +47,12 @@ const AppComponent: React.FC = () =>{
       <NavLayout>
         <HeaderLayout>
           {ParseHeader(location.pathname)}
+          <Select
+          styles={ChoiceStyles}
+          value={selectedOp}
+          onChange={handleChange}
+          options={tempOption}
+          />
         </HeaderLayout>
         <Routes>
           <Route path="/" element={'NEED LOGIN PAGE'}/>
