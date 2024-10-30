f2e5c623bc417d93c6074504115032f0d218948b
hkk3626 <hkk3626@naver.com>
2024-10-30 09:55:08 +0900

    :sparkles: Base파일 수정 및 세팅(S11P31S106-13)

--- backend/src/main/java/com/core/backend/BackendApplication.java
+++ backend/src/main/java/com/core/backend/BackendApplication.java
@@ -2,12 +2,17 @@ package com.core.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaAuditing
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 
 }
