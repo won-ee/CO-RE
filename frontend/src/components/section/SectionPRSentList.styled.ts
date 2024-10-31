ad2d30532bc47b80d05d3ba058d7fa699a0a6704
강동형 <buslaut@naver.com>
2024-10-31 10:52:12 +0900

    :Sparkles: Sent 탭 표 내용 구성 1차 완료

--- frontend/src/components/section/SectionPRSentList.styled.ts
+++ frontend/src/components/section/SectionPRSentList.styled.ts
@@ -3,10 +3,9 @@ import styled from 'styled-components';
 interface GridCellProps {
     align?: 'left' | 'center'; // 정렬 방식 지정 (left 또는 center)
   }
-interface StatusBoxProps {
+interface StringProps {
     status:string;
 }
-
 // Section과 Filter 레이아웃
 export const SectionPRSentListLayout = styled.div`
 `;
@@ -29,13 +28,17 @@ export const GridTable = styled.div`
 // Header
 export const GridHeader = styled.div`
   display: contents;
+  
 `;
 
 export const GridHeaderCell = styled.div<GridCellProps>`
   padding: 15px 41px;
   font-weight: bold;
-  /* border-bottom: 0.6px solid #B9B9B9; */ //헤더 가로선
-  text-align: ${(props) => (props.align ? props.align : 'left')};
+  /* border-bottom: 0.6px solid #B9B9B9; //헤더 가로선 */
+  display: flex;
+  align-items: center;
+  justify-content: ${(props) => (props.align ? props.align : 'left')};
+  box-sizing: border-box;
 `;
 
 // Row와 Cell
@@ -52,48 +55,77 @@ export const GridCell = styled.div<GridCellProps>`
   text-align: ${(props) => (props.align ? props.align : 'left')};
 `;
 export const CommentBox = styled.div`
-    background-color: #D9D9D9;
-    width: 25px;
-    height: 25px;
-    display: flex;
-    align-items: center; /* 수직 가운데 정렬 */
-    justify-content: center; /* 수평 가운데 정렬 */
-    text-align: center;
-    border-radius: 100%;
+  background-color: #D9D9D9;
+  width: 25px;
+  height: 25px;
+  display: flex;
+  align-items: center; /* 수직 가운데 정렬 */
+  justify-content: center; /* 수평 가운데 정렬 */
+  text-align: center;
+  border-radius: 100%;
 `
-export const StatusBox = styled.div<StatusBoxProps>`
-    width: 93px;
-    height: 27px;
-    font-size: 12px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-weight: bold;
-    border-radius: 4px;
-    background-color: ${(props) => {
-    switch (props.status) {
-      case 'Approved':
-        return '#CCF0EB'; 
-      case 'Processing':
-        return '#E0D4FC'; 
-      case 'Rejected':
-        return '#FCD7D4'; 
-      default:
-        return 'white'; // 기본 배경색
-    }
-    }};
-    color: ${(props) => {
-    switch (props.status) {
-      case 'Approved':
-        return '#00B69B'; 
-      case 'Processing':
-        return '#6226EF'; 
-      case 'Rejected':
-        return '#EF3826'; 
-      default:
-        return 'white'; // 기본 배경색
-    }
-    }};
+export const StatusBox = styled.div<StringProps>`
+  width: 93px;
+  height: 27px;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  border-radius: 4px;
+  background-color: ${(props) => {
+  switch (props.status) {
+    case 'Approved':
+      return '#CCF0EB'; 
+    case 'Processing':
+      return '#E0D4FC'; 
+    case 'Rejected':
+      return '#FCD7D4'; 
+    default:
+      return 'white'; // 기본 배경색
+  }
+  }};
+  color: ${(props) => {
+  switch (props.status) {
+    case 'Approved':
+      return '#00B69B'; 
+    case 'Processing':
+      return '#6226EF'; 
+    case 'Rejected':
+      return '#EF3826'; 
+    default:
+      return 'white'; // 기본 배경색
+  }
+  }};
 `
-export const PriorityBox = styled.div`
+export const PriorityBox = styled.div<StringProps>`
+  width: 93px;
+  height: 27px;
+  font-size: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: bold;
+  border-radius: 4px;
+  color: white;
+  background-color: ${(props) => {
+  switch (props.status) {
+    case 'HIGH':
+      return '#FF0000'; 
+    case 'MIDDLE':
+      return '#67C658'; 
+    case 'LOW':
+      return '#78C7FF'; 
+    default:
+      return 'white'; // 기본 배경색
+  }
+  }};
+`
+export const DeadLineBox = styled.div<StringProps>`
+  font-weight: bold;
+  white-space: nowrap;
+  color: ${(props)=>(props.status=="After Review" ? '#723CF1': parseInt(props.status.replace('D-',''),10) < 2 ? 'red':'black')};
+`
+export const TitleBox = styled.div`
+  font-weight: bold;
 `
--- frontend/src/components/section/SectionPRSentList.tsx
+++ frontend/src/components/section/SectionPRSentList.tsx
@@ -1,9 +1,9 @@
-import { SectionPRSentListLayout,FilterLayout, GridTable , GridHeader, GridHeaderCell, GridRow, GridCell,CommentBox,StatusBox } from './SectionPRSentList.styled'
+import { SectionPRSentListLayout,FilterLayout, GridTable , GridHeader, GridHeaderCell, GridRow, GridCell,CommentBox,StatusBox,PriorityBox, DeadLineBox, TitleBox} from './SectionPRSentList.styled'
 
 const data = [
-  { col1: 'Frame Adjusement', col2: 'Frame into frontend', col3: 'D-1', col4: 'Row 1 Col 4', col5: '5', col6: 'HIGH', col7: 'Approved' },
-  { col1: 'Row 2 Col 1', col2: 'Row 2 Col 2', col3: 'Row 2 Col 3', col4: 'Row 2 Col 4', col5: '21', col6: 'MIDDLE', col7: 'Processing' },
-  { col1: 'Row 3 Col 1', col2: 'Row 3 Col 2', col3: 'Row 3 Col 3', col4: 'Row 3 Col 4', col5: '3', col6: 'LOW', col7: 'Rejected' },
+  { col1: 'Frame Adjusement', col2: 'Frame into frontend', col3: 'D-1', col4: 'IMAGES', col5: '5', col6: 'HIGH', col7: 'Approved' },
+  { col1: 'Sound Add', col2: 'Adjustment into frontend', col3: 'D-2', col4: 'IMAGES', col5: '21', col6: 'MIDDLE', col7: 'Processing' },
+  { col1: 'Schedule Page total Direct & Create', col2: 'Row 3 Col 2', col3: 'After Review', col4: 'IMAGES', col5: '3', col6: 'LOW', col7: 'Rejected' },
 ];
 
 function SectionPRSentList() {
@@ -28,15 +28,19 @@ function SectionPRSentList() {
 
         {/* Body (Rows) */}        {data.map((row, index) => (
           <GridRow key={index}>
-            <GridCell>{row.col1}</GridCell>
+            <GridCell>
+              <TitleBox>{row.col1}</TitleBox>
+              </GridCell>
             <GridCell>{row.col2}</GridCell>
-            <GridCell align="center">{row.col3}</GridCell>
+            <GridCell align="center">
+              <DeadLineBox status={row.col3}>{row.col3}</DeadLineBox>
+              </GridCell>
             <GridCell>{row.col4}</GridCell>
             <GridCell align="center">
               <CommentBox>{row.col5}</CommentBox>
             </GridCell>
             <GridCell align="center">
-              {row.col6}
+              <PriorityBox status={row.col6}>{row.col6}</PriorityBox>
               </GridCell>
             <GridCell align="center">
               <StatusBox status={row.col7}>{row.col7}</StatusBox>
