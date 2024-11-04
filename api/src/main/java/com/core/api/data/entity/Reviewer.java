4352e2197f6af1e3bb5ca8794a67fbcfcc926b49
정민 <jmeve24@naver.com>
2024-11-04 17:48:55 +0900

    ♻️ 컨벤션 맞게 수정(S11P31S106-117)

--- api/src/main/java/com/core/api/data/entity/Reviewer.java
+++ api/src/main/java/com/core/api/data/entity/Reviewer.java
@@ -5,21 +5,22 @@ import lombok.Getter;
 
 @Entity
 @Getter
-public class Reviewer extends  Base{
+public class Reviewer extends Base {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "reviewer_id", nullable = false)
     private Long id;
 
-    @Column(name="reviewer_user_id", nullable = false)
+    @Column(name = "reviewer_user_id", nullable = false)
     private String reviewerId;
 
-    @Column(name="reviewer_score")
-    private int  score;
+    @Column(name = "reviewer_score")
+    private int score;
 
     @ManyToOne
-    @JoinColumn(name="pr_id", nullable = false)
+    @JoinColumn(name = "pr_id", nullable = false)
     private PullRequest pullRequest;
+
 
 }
