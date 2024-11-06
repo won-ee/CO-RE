5accf93115d245ddf8131c7d2048eabd6ba4b23a
정민 <jmeve24@naver.com>
2024-11-06 17:02:02 +0900

    ♻️  API 재사용을 위해서 경로 변경(S11P31S106-121)

--- api/src/main/java/com/core/api/controller/PullRequestController.java
+++ api/src/main/java/com/core/api/controller/PullRequestController.java
@@ -5,9 +5,11 @@ import com.core.api.data.dto.commit.CommitMessageDto;
 import com.core.api.data.dto.pullrequest.PullRequestDateFilterDto;
 import com.core.api.data.dto.pullrequest.PullRequestDto;
 import com.core.api.data.dto.pullrequest.PullRequestFilterDto;
+import com.core.api.data.dto.pullrequest.PullRequestInputDto;
 import com.core.api.data.dto.response.MergeResponseDto;
 import com.core.api.service.PullRequestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,14 +22,14 @@ public class PullRequestController {
 
     private final PullRequestService pullRequestService;
 
-    @GetMapping("/{owner}/{repo}/{pullId}/files")
-    public ResponseEntity<List<ChangeDto>> getChangeFiles(@PathVariable String owner, @PathVariable String repo, @PathVariable int pullId) {
-        List<ChangeDto> changeFiles = pullRequestService.getChangeFiles(owner, repo, pullId);
+    @GetMapping("/{owner}/{repo}/files/{baseHead}")
+    public ResponseEntity<List<ChangeDto>> getChangeFiles(@PathVariable String owner, @PathVariable String repo, @PathVariable String baseHead) {
+        List<ChangeDto> changeFiles = pullRequestService.getChangeFiles(owner, repo, baseHead);
         return ResponseEntity.ok(changeFiles);
     }
 
     @GetMapping("/{owner}/{repo}")
-    public ResponseEntity<List<PullRequestDto>> getPullRequestList(@PathVariable String owner, @PathVariable String repo) {
+    public ResponseEntity<List<PullRequestDto>> getPullRequestList(Pageable pageable, @PathVariable String owner, @PathVariable String repo) {
         List<PullRequestDto> pullRequestList = pullRequestService.getPullRequestList(owner, repo);
         return ResponseEntity.ok(pullRequestList);
     }
