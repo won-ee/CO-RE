0710b14c9c4735a8efe6f9468cdda556f22fcc88
unknown <ks00919@naver.com>
2024-11-11 15:44:44 +0900

    :recycle: cookie util private structor

--- backend/src/main/java/com/core/backend/util/CookieUtil.java
+++ backend/src/main/java/com/core/backend/util/CookieUtil.java
@@ -6,6 +6,10 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CookieUtil {
 
+    private CookieUtil() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static Cookie createCookie(String key, String value, int maxAge, boolean secure) {
         Cookie cookie = new Cookie(key, value);
         cookie.setHttpOnly(true);
