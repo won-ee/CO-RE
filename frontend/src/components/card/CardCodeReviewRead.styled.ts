4cc50fc356200ac0659e6033aa643321102e8c9e
강동형 <buslaut@naver.com>
2024-11-13 18:01:10 +0900

    :construction: 카드코드리뷰리드 페이지 구성중(S11P31S106-68)

--- frontend/src/components/card/CardCodeReviewRead.styled.ts
+++ frontend/src/components/card/CardCodeReviewRead.styled.ts
@@ -1,9 +1,33 @@
+import exp from "constants";
 import styled from "styled-components";
 
 export const CardCodeReviewReadLayout = styled.div`
     padding: 10px;
 `
 export const CardCodeReviewReadBox = styled.div`
+    display: flex;
     border: 1px solid black;
     border-radius: 5px;
+    gap: 10px;
+    padding: 14px;
+`
+export const ContentBox = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+`
+export const NameBox = styled.div`
+    display: flex;
+    gap: 10px;
+`
+export const NameSpan = styled.span`
+    font-size: 14px;
+    font-weight: 600;
+`
+export const HourSpan = styled.span`
+    font-size: 14px;
+    font-weight: 300;
+`
+export const BodyBox = styled.div`
+    font-size: 14px;
 `
