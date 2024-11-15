b62ce318a9ac10775e2622f67ba71311e807fbcf
지민경 <t01045239155@gmail.com>
2024-11-15 14:16:49 +0900

    :sparkles: Dashboard Stats Card API 연결

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -6,17 +6,19 @@ import MainIssue from "../components/DashIssue/MainIssue";
 import LoadingPage from "./LoadingPage";
 import NotFoundPage from "./NotFoundPage";
 
-// import { useDashboard, useDashPR } from "../hooks/useDashboard";
-import { useDashPR } from "../hooks/useDashboard";
+import { useDashboard, useDashPR } from "../hooks/useDashboard";
 import { useProjectStore } from "../store/userStore";
 
 const Dashboard: React.FC = () => {
   const projectInfo = useProjectStore((state) => state);
-  // const {
-  //   data: dashboardData,
-  //   isLoading: isDashboardLoading,
-  //   error: dashboardError,
-  // } = useDashboard();
+  const {
+    data: dashboardData,
+    isLoading: isDashboardLoading,
+    error: dashboardError,
+  } = useDashboard({
+    owner: projectInfo.selectedOwner,
+    repo: projectInfo.selectedRepo,
+  });
 
   const {
     data: dashPRData,
@@ -28,18 +30,13 @@ const Dashboard: React.FC = () => {
     state: "receive",
   });
 
-  // if (isDashboardLoading || isDashPRLoading) return <LoadingPage />;
-  // if (dashboardError || dashPRError) return <NotFoundPage />;
-  // if (!dashboardData || !dashPRData) return null;
-
-  if (isDashPRLoading) return <LoadingPage />;
-  if (dashPRError) return <NotFoundPage errorNumber={404} />;
-  if (!dashPRData) return null;
+  if (isDashboardLoading || isDashPRLoading) return <LoadingPage />;
+  if (dashboardError || dashPRError) return <NotFoundPage errorNumber={404} />;
+  if (!dashboardData || !dashPRData) return null;
 
   return (
     <div>
-      {/* <MainStats data={dashboardData} /> */}
-      <MainStats />
+      <MainStats data={dashboardData} />
       <FilterAndGraphSection />
       <div>
         <MainPR data={dashPRData} />
