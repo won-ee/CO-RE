e1def217a88083bd97eca5692b1aadcbc0e7d98e
정민 <jmeve24@naver.com>
2024-11-16 13:17:49 +0900

    ✨ chatgpt api open feign 설정(S11P31S106-119)

--- api/src/main/java/com/core/api/config/ChatGptFeignConfig.java
+++ api/src/main/java/com/core/api/config/ChatGptFeignConfig.java
@@ -8,22 +8,19 @@ import feign.codec.Encoder;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import feign.okhttp.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
+public class ChatGptFeignConfig {
 
-@Configuration
-public class FeignConfig {
+    @Value("${chatgpt.api.token}")
+    private String token;
 
     @Bean
     public RequestInterceptor requestInterceptor() {
-
-        // TODO : 시큐리티 토큰값으로 변경
-        String token = "";
         return requestTemplate -> {
             requestTemplate.header("Authorization", "Bearer " + token);
-            requestTemplate.header("X-GitHub-Api-Version", "2022-11-28");
-            requestTemplate.header("Accept", "application/vnd.github+json; charset=UTF-8");
+            requestTemplate.header("Content-Type", "application/json");
         };
     }
 
