5fd6c1d773e1862cce7679985017252a67cce876
정민 <jmeve24@naver.com>
2024-11-16 13:14:07 +0900

    ✨ 토큰 필터 적용(S11P31S106-119)

---
+++ api/src/main/java/com/core/api/filter/AuthenticationFilter.java
@@ -0,0 +1,54 @@
+package com.core.api.filter;
+
+import com.core.api.service.AuthService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final AuthService authService;
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String accessToken = request.getHeader("Authorization");
+        if (accessToken == null) {
+            throw new AccessDeniedException("Access token is missing");
+        }
+
+        if (SecurityContextHolder.getContext()
+                .getAuthentication() == null) {
+
+            String githubToken = authService.requestTokenFromServer(accessToken)
+                    .token();
+
+
+            if (githubToken == null) {
+                throw new AccessDeniedException("Github access token is missing");
+            }
+
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(accessToken, null, Collections.emptyList());
+            auth.setDetails(githubToken);
+            SecurityContextHolder.getContext()
+                    .setAuthentication(auth);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+}
