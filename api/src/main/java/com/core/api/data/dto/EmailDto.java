b2db5fe4cc12922769ee953ea302c9d688e209a0
정민 <jmeve24@naver.com>
2024-11-18 17:41:38 +0900

    🐛 카멜케이스 강제적용(S11P31S106-123)

--- api/src/main/java/com/core/api/data/dto/EmailDto.java
+++ api/src/main/java/com/core/api/data/dto/EmailDto.java
@@ -1,14 +1,15 @@
 package com.core.api.data.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record EmailDto(
-        List<String> to,
-        String projectName,
-        List<String> contents,
-        Integer totalCommit,
-        Integer totalPullRequest,
-        Integer totalReview
+        @JsonProperty("to") List<String> to,
+        @JsonProperty("projectName") String projectName,
+        @JsonProperty("contents") List<String> contents,
+        @JsonProperty("totalCommit") Integer totalCommit,
+        @JsonProperty("totalPullRequest") Integer totalPullRequest,
+        @JsonProperty("totalReview") Integer totalReview
 ) {
-
 }
