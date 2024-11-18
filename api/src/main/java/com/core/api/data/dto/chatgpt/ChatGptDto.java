c11f91d3e0513c31dfec3b3b6f3c98e54b77595a
정민 <jmeve24@naver.com>
2024-11-19 01:21:50 +0900

    ⚡️ 프롬프트 변경(S11P31S106-123)

--- api/src/main/java/com/core/api/data/dto/chatgpt/ChatGptDto.java
+++ api/src/main/java/com/core/api/data/dto/chatgpt/ChatGptDto.java
@@ -16,7 +16,9 @@ public record ChatGptDto(
                 .map(CompareBranchResponseDto::message)
                 .collect(Collectors.joining("\n"));
 
-        String prompt = template + " 다음 커밋 메시지를 분석하여 템플릿 양식을 한글로 작성해 주세요. 마크 다운 형식으로 작성해주세요. 템플릿에 대한 설명이나 안내 문구는 포함하지 마세요." + commitsSummary;
+        String prompt = "다음 커밋 메시지를 분석하여 아래 템플릿 양식에 맞게 작성해 주세요. " +
+                "아래 템플릿에 대한 수정 없이, 그대로 따라 작성하세요. " +
+                "템플릿 외의 설명이나 안내 문구는 포함하지 마세요.\n\n" + template;
 
         ChatGptMessageDto system = new ChatGptMessageDto("system", prompt);
         ChatGptMessageDto user = new ChatGptMessageDto("user", commitsSummary);
